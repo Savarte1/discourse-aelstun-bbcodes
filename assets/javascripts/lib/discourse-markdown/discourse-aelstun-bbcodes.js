c76971012313e97766fce5a1a9f7d2ccfda0eddc
Savarte1 <artevenia@gmail.com>
2023-02-26 18:52:17 -0600

    Add code

---
+++ assets/javascripts/lib/discourse-markdown/discourse-aelstun-bbcodes.js
@@ -0,0 +1,54 @@
+function setupMarkdownIt(md){
+    const inline_ruler = md.inline.bbcode.ruler;
+    const block_ruler = md.block.bbcode.ruler;
+
+    block_ruler.push('box', {
+        tag: 'box',
+        wrap: 'div.af-box'
+    });
+
+    inline_ruler.push('nation', {
+        tag: 'nation',
+        wrap: function(startToken, endToken, tagInfo, content) {
+           const url = (tagInfo.attrs['_default'] || content).trim();
+           startToken.type = 'link_open';
+           startToken.tag = 'a';
+           startToken.attrs = [['href', 'https://www.nationstates.net/nation=' + url], ['data-bbcode', 'true']];
+           startToken.content = '';
+           startToken.nesting = 1;
+
+           endToken.type = 'link_close';
+           endToken.tag = 'a';
+           endToken.content = '';
+           endToken.nesting = -1;
+        }
+     });
+
+     inline_ruler.push('region', {
+        tag: 'region',
+        wrap: function(startToken, endToken, tagInfo, content) {
+           const url = (tagInfo.attrs['_default'] || content).trim();
+           startToken.type = 'link_open';
+           startToken.tag = 'a';
+           startToken.attrs = [['href', 'https://www.nationstates.net/region=' + url], ['data-bbcode', 'true']];
+           startToken.content = '';
+           startToken.nesting = 1;
+
+           endToken.type = 'link_close';
+           endToken.tag = 'a';
+           endToken.content = '';
+           endToken.nesting = -1;
+        }
+     });
+
+}
+
+export function setup(helper) {
+    if(!helper.markdownIt) { return; }
+
+    helper.allowList([
+        'div.af-box'
+    ]);
+
+    helper.registerPlugin(setupMarkdownIt);
+}
